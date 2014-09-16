82e57ca7d6416150428fc491d79b060010def806
netphager <netphager@gmail.com>
2014-09-16 11:25:25 +0300

    adding dialog

--- dialog.js
+++ dialog.js
@@ -2,8 +2,19 @@ define(function() {
     return new (function() {
         var that = this;
 
-        this.init = function() {
+        this.init = function() {};
 
+        this.open = function(template) {
+            // load dialog template
+            that.makeRequest({
+                type:'post',
+                url:'/loadTemplate',
+                data: {
+                    "templateName":'dialog'
+                }
+            },function(template) {
+
+            });
         };
 
     });
