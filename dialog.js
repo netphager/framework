69fe0158aec1ebd97b84df5bdfa1c9e96dfd3a22
netphager <netphager@gmail.com>
2014-09-29 15:39:51 +0300

    close dialog with esc key

--- dialog.js
+++ dialog.js
@@ -4,17 +4,27 @@ define(function() {
         var router = require('helper/router');
         this.init = function() {};
 
+        // open dialog
         this.open = function(templateName,params) {
             // load dialog template
-            var dialogHtml = router.render('dialog');
-            dialogHtml = dialogHtml.replace('class="dialog"','class="dialog '+templateName+'"');
-            $('[dialog-template]').after(dialogHtml);
+            $('[dialog-template]').after(router.render('dialog').replace('class="dialog"','class="dialog '+templateName+'"'));
             $('.dialog.'+templateName+' .content').html(router.render(templateName,params));
-            $('.dialog.'+templateName+' .close').click(function() {that.close(templateName)});
+
+            // close dialog event listeners
+            $('.dialog.'+templateName+' .close').on('click',function() {that.close(templateName)});
+            // close dialog with esc key
+            $(window).on('keyup',function(e) {
+                if(e.keyCode != 27) {
+                    return;
+                }
+                that.close(templateName);
+            });
         };
 
+        // close dialog
         this.close = function(dialogName) {
             var $dialog = $('.dialog.'+dialogName);
+            $(window).off('keyup');
             $dialog.remove();
             window.location = '/app/#/article/home';
         };
